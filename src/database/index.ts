d954042be71d505d20d05b626622f79db2091d7e
LuigiBelanda <luigi.belanda.milani@gmail.com>
2022-03-15 12:49:52 -0300

    Entendendo o que sao as migrations / Usando migrations

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,3 +2,11 @@ import { createConnection } from "typeorm";
 
 // criando a conexão com o bando de dados
 createConnection();
+
+// COMANDO BD
+/**
+ * yarn typeorm -help
+ * yarn typeorm migration:create -n CreateUsers
+ * yarn typeorm migration:run (roda as migrations)
+ * yarn typeorm migration:revert (reverte a última migration rodada)
+ */
